642cdbdb1dbc600740eb60f67627b7aeb8989fa0
jackdon <janeysesions@gmail.com>
2017-09-12 10:31:19 +0800

    table demo

--- src/app/table/person-data-source.ts
+++ src/app/table/person-data-source.ts
@@ -8,7 +8,7 @@ import { PeopleDatabase, UserData } from './people-database';
 import 'rxjs/add/observable/merge';
 import 'rxjs/operator/map';
 
-export class PeopleDataSource extends DataSource<any> {
+export class PersonDataSource extends DataSource<any> {
 
   constructor(
     private _peopleDatabase: PeopleDatabase,
@@ -24,11 +24,39 @@ export class PeopleDataSource extends DataSource<any> {
       this._sort.mdSortChange,
       this._peopleDatabase.dataChange
     ];
-    return null;
+
+    return Observable.merge(...displayDataChanges).map(() => {
+      const data = this.getSortedData();
+      
+      // Grab the page's slice of data
+      const startIndex = this._paginator.pageIndex * this._paginator.pageSize;
+      return data.splice(startIndex, this._paginator.pageSize);
+    });
   }
 
   disconnect():void {
     // no op
+  }
+
+  getSortedData(): UserData[] {
+    const data = this._peopleDatabase.data.slice();
+    if (!this._sort.active || this._sort.direction == '') { return data }
+
+    return data.sort((a, b) => {
+      let propertyA: number|string = '';
+      let propertyB: number|string = '';
+
+      switch (this._sort.active) {
+        case 'userId': [propertyA, propertyB] = [a.id, b.id]; break;
+        case 'userName': [propertyA, propertyB] = [a.id, b.id]; break;
+        case 'progress': [propertyA, propertyB] = [a.id, b.id]; break;
+        case 'color': [propertyA, propertyB] = [a.id, b.id]; break;
+      }
+
+      let valueA = isNaN(+propertyA)? propertyA: +propertyA;
+      let valueB = isNaN(+propertyB)? propertyB: +propertyB;
+      return (valueA < valueB? -1 : 1) * (this._sort.direction == 'asc'? 1 : -1);
+    })
   }
 
 }
--- src/app/table/table-demo.ts
+++ src/app/table/table-demo.ts
@@ -1,3 +1,48 @@
 /**
  * Created by xulingming on 2017/9/10.
  */
+import {Component, ViewEncapsulation, ViewChild, OnInit } from '@angular/core';
+import { PeopleDatabase, UserData } from 'people-database';
+import { PersonDataSource } from 'person-data-source';
+import { MdPaginator, MdSort } from '@angular/material';
+
+export type UserProperties = 'userId' | 'userName' | 'progress' | 'color' | undefined;
+export type TrackByStrategy = 'id' | 'reference' | 'index';
+
+const properties = ['id', 'name', 'progress', 'color'];
+
+@Component(
+  {
+    moduleId: module.id,
+    selector: 'table-demo',
+    templateUrl: 'table-demo.html',
+    styleUrls: ['table-demo.scss']
+  }
+)
+export class TableDemo implements OnInit {
+  dataSource: PersonDataSource = null;
+  displayColumns: UserProperties[] = [];
+  trackByStrategy: TrackByStrategy = 'reference';
+  changeReferences = false;
+  highlights = new Set<string>();
+
+  dynamicColumnDefs: any[] = [];
+  dynamicColumnIds: string[] = [];
+  
+  @ViewChild(MdPaginator) _paginator: MdPaginator;
+  
+  @ViewChild(MdSort) sort: MdSort;
+  
+  constructor(public _peopleDatabase: PeopleDatabase) {
+  }
+
+
+  ngOnInit():void {
+  }
+  
+  connect(): void {
+    this.displayColumns = ['userId', 'userName', 'progress', 'color'];
+    this.dataSource = new PersonDataSource(this._peopleDatabase, this._paginator, this.sort);
+    this._peopleDatabase.initialize();
+  }
+}
